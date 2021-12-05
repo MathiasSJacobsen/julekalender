6e81301965e96931d63d245655bfbcc4e9100bdb
Mathias <mathias.skallerud@gmail.com>
2021-12-05 23:27:57 +0100

    :santa: silver check

--- 2021/AoC/luke5.py
+++ 2021/AoC/luke5.py
@@ -2,9 +2,9 @@
     
 def makeMap():
     map = []
-    for _ in range(10):
+    for _ in range(1000):
         x = []
-        for _ in range(10):
+        for _ in range(1000):
             x.append(0)
         map.append(x)
     return map
@@ -12,10 +12,11 @@ def makeMap():
 def filterLines(lines):
     temp = []
     for e in lines:
-        x1 = e[0].strip()[0]
-        x2 = e[1].strip()[0]
-        y1 = e[0].strip()[2]
-        y2 = e[1].strip()[2]
+        
+        x1 = e[0].strip().split(',')[0]
+        x2 = e[1].strip().split(',')[0]
+        y1 = e[0].strip().split(',')[1]
+        y2 = e[1].strip().split(',')[1]
         if x1 == x2 or y1 == y2:
             t1 = [(x1, y1), (x2, y2)]
             temp.append(t1)
@@ -23,31 +24,39 @@ def filterLines(lines):
 
 
 if __name__ == '__main__':
-    lines = [e.split("->") for e in open('test.txt').readlines()]
+    lines = [e.split("->") for e in open('luke5silver.txt').readlines()]
     
     filterdLines = filterLines(lines)
     map = makeMap()
 
     for e in filterdLines:
+        print(e)
         x1 = int(e[0][0])
         x2 = int(e[1][0])
         y1 = int(e[0][1])
         y2 = int(e[1][1])
         #if x1 < x2:
             #print(x1, y1, x2, y2)
-        while x1 <= x2:
+        while x1 < x2:
             map[y1][x1] = map[y1][x1] + 1
             x1 += 1
             continue
-        while x1 >= x2:
+        while x1 > x2:
             map[y1][x1] = map[y1][x1] + 1
             x1 -= 1
             continue
-        while y1 <= y2:
+        while y1 < y2:
             map[y1][x1] = map[y1][x1] + 1
             y1 += 1
             continue
+        while y1 >= y2:
+            map[y1][x1] = map[y1][x1] + 1
+            y1 -= 1
+            continue
 
-    print(filterdLines)
+    s = 0
     for e in map:
-        print(e)
+        for i in e:
+            if i > 1:
+                s +=1
+    print(s)
